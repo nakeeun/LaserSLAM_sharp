8b226436eb1f027307c6e4a24ad11002827374a8
Nakeun Choi <37968684+nakeeun@users.noreply.github.com>
2022-02-07 15:02:41 +0900

    Update SLAM_utils.cs

--- LaserSLAM_sharp/SLAM_utils.cs
+++ LaserSLAM_sharp/SLAM_utils.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Text;
 using YDLidarSharp;
 
 class SLAM_utils
 {
-     public static double[,] create_BWmap(double[,] localMap, double[,] min_max_val, double pixel_size) 
-     {
+    public static double[,] create_BWmap(double[,] localMap, double[,] min_max_val, double pixel_size) {
         int map_size = localMap.GetLength(0);
         int[] sgrid = SLAM_utils.extract_sgrid(min_max_val, pixel_size);
         int[] idx = SLAM_utils.cal_idx(localMap, min_max_val, sgrid, pixel_size, map_size);
@@ -38,7 +38,7 @@ class SLAM_utils
                         for (int n = 0; n < p_y; n++)
                         {
                             double distance = Math.Sqrt(Math.Pow(m - i, 2) + Math.Pow(n - j, 2));
-                            distance = Math.Min(distance, 10);
+                            distance = Math.Min(distance, 20);
                             if (first == false)
                             {
                                 BWmap[m, n] = distance;
@@ -59,16 +59,18 @@ class SLAM_utils
         }
         return BWmap;
     }
-    public static int[] extract_sgrid(double[,] min_max_val, double pixelSize, int[] sgrid)
+    public static int[] extract_sgrid(double[,] min_max_val, double pixelSize)
     {
+        int[] sgrid = new int[2];
         sgrid[0] = (int)(Math.Round((min_max_val[1, 0] - min_max_val[0, 0]) / pixelSize) + 1);
         sgrid[1] = (int)(Math.Round((min_max_val[1, 1] - min_max_val[0, 1]) / pixelSize) + 1);
         return sgrid;
     }
-    public static int[] cal_idx(double[,] localMap, double[,] min_max_val, int[] sgrid, double pixelSize, int map_size, int[] idx)
+    public static int[] cal_idx(double[,] localMap, double[,] min_max_val, int[] sgrid, double pixelSize, int map_size)
     {
+        int[] idx = new int[map_size];
         int[,] hits = new int[map_size, 2];
-        for (int i = 0; i < map_size; i++)
+        for(int i=0; i< map_size; i++)
         {
             hits[i, 0] = (int)(Math.Round((localMap[i, 0] - min_max_val[0, 0]) / pixelSize) + 1);
             hits[i, 1] = (int)(Math.Round((localMap[i, 1] - min_max_val[0, 1]) / pixelSize) + 1);
@@ -78,34 +80,33 @@ class SLAM_utils
     }
     public static double[,] ExtractLocalMap(double[,] map_points, double[,] scan, double[] pos)
     {
-        double[,] scan_w = new double[scan.GetLength(0), 2];
         double[,] min_max_val = new double[2, 2];
-        scan_w = transform(scan, pos, scan.GetLength(0), scan_w);
+        double[,] scan_w = transform(scan, pos, scan.GetLength(0));
         double minX = 99999999, minY = 99999999, maxX = -99999999, maxY = -99999999;
-        for (int i = 0; i < scan.GetLength(0); i++)
+        for(int i= 0; i < scan.GetLength(0); i++)
         {
-            if (scan_w[i, 0] - 1 < minX)
-                minX = scan_w[i, 0] - 1;
-            if (scan_w[i, 1] - 1 < minY)
-                minY = scan_w[i, 1] - 1;
-            if (scan_w[i, 0] + 1 > maxX)
-                maxX = scan_w[i, 0] + 1;
-            if (scan_w[i, 1] + 1 > maxY)
-                maxY = scan_w[i, 1] + 1;
-        }
+            if (scan_w[i, 0] < minX)
+                minX = scan_w[i, 0] - 0.2;
+            if (scan_w[i, 1] < minY)
+                minY = scan_w[i, 1] - 0.2;
+            if (scan_w[i, 0] > maxX)
+                maxX = scan_w[i, 0] + 0.2;
+            if (scan_w[i, 1] > maxY)
+                maxY = scan_w[i, 1] + 0.2;
+        };
         int cnt = 0;
         double[,] temp_localMap = new double[map_points.GetLength(0), 2];
         for (int i = 0; i < map_points.GetLength(0); i++)
         {
-            if (map_points[i, 0] > minX && map_points[i, 0] < maxX && map_points[i, 1] > minY && map_points[i, 1] < maxY)
-            {
-                temp_localMap[cnt, 0] = map_points[i, 0];
-                temp_localMap[cnt, 1] = map_points[i, 1];
-                cnt++;
-            }
+           if (map_points[i, 0] > minX && map_points[i, 0] < maxX && map_points[i, 1] > minY && map_points[i, 1] < maxY)
+           {
+               temp_localMap[cnt, 0] = map_points[i, 0];
+               temp_localMap[cnt, 1] = map_points[i, 1];
+               cnt++;
+           }
         }
         double[,] localMap = new double[cnt, 2];
-        for (int i = 0; i < cnt; i++)
+        for(int i=0; i< cnt; i++)
         {
             localMap[i, 0] = temp_localMap[i, 0];
             localMap[i, 1] = temp_localMap[i, 1];
@@ -140,14 +141,33 @@ class SLAM_utils
     public static double[,] pol2cart(LaserScan scan, double[,] p2c)
     {
         int scan_size = scan.LaserPoints.Count;
-            
-        for (int i = 0; i < scan_size; i++)
+
+        for(int i=0; i<scan_size; i++)
         {
-            double angle = (double)scan.LaserPoints[i].Angle;
-            double range = (double)scan.LaserPoints[i].Range;
+              double angle = (double)scan.LaserPoints[i].Angle;
+              double range = (double)scan.LaserPoints[i].Range;
+              p2c[i, 0] = range * Math.Cos(angle);
+              p2c[i, 1] = range * Math.Sin(angle);
+        }
+        return p2c;
+    }
+    public static double[,] double_pol2cart(LaserScan scan_f, LaserScan scan_r, double[,] p2c)
+    {
+        int scan_size_f = scan_f.LaserPoints.Count;
+        for (int i = 0; i < scan_size_f; i++)
+        {
+            double angle = (double)scan_f.LaserPoints[i].Angle;
+            double range = (double)scan_f.LaserPoints[i].Range;
             p2c[i, 0] = range * Math.Cos(angle);
             p2c[i, 1] = range * Math.Sin(angle);
-            //write_data(savePath, p2c[i, 0], p2c[i, 1]);
+        }
+        int scan_size_r = scan_r.LaserPoints.Count;
+        for (int i = 0; i < scan_size_r; i++)
+        {
+            double angle = (double)scan_r.LaserPoints[i].Angle;
+            double range = (double)scan_r.LaserPoints[i].Range;
+            p2c[i+ scan_size_f, 0] = range * Math.Cos(angle);
+            p2c[i+ scan_size_f, 1] = range * Math.Sin(angle);
         }
         return p2c;
     }
@@ -174,19 +194,19 @@ class SLAM_utils
     public static double rad2deg(double radian)
     {
         double degree = 0.0;
-        degree = radian * 180 / Math.PI;
+        degree = radian * 180.0/Math.PI;
         return radian;
     }
-    public static double[] diffPos(double[] prev_pos, double[] pos, double[] dp)
+    public static double[] diffPos(double[] prev_pos, double[] pos)
     {
+        double[] dp = new double[3];
         dp[0] = pos[0] - prev_pos[0];
         dp[1] = pos[1] - prev_pos[1];
-        //dp[2] = Math.PI - Math.Abs(Math.Abs(pos[2] - prev_pos[2]) - Math.PI);
-        dp[2] = Math.Atan2(Math.Sin(prev_pos[2] - pos[2]), Math.Cos(prev_pos[2] - pos[2])); // 이 식 재 검토 필요
+        dp[2] = Math.Atan2(Math.Sin(prev_pos[2] - pos[2]), Math.Cos(prev_pos[2] - pos[2]));
 
         return dp;
     }
-    public static double[,] min_max_func(double[,] points, int scan_size, double[,] min_max_value)
+    public static double[,] min_max_func(double[,] points, int scan_size)
     {
         double minX = 99999999, minY = 99999999, maxX = -99999999, maxY = -99999999;
         for (int i = 0; i < scan_size; i++)
@@ -200,6 +220,7 @@ class SLAM_utils
             if (points[i, 1] > maxY)
                 maxY = points[i, 1];
         }
+        double[,] min_max_value = new double[2, 2];
         min_max_value[0, 0] = minX;
         min_max_value[0, 1] = minY;
         min_max_value[1, 0] = maxX;
